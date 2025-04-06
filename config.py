418f5efaedcc0b0d0c5e55c136f8bf7450a08ddd
Kavita-725 <dkavita2121@gmail.com>
2025-04-06 19:28:00 +1000

    init

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "default_secret_key")
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/journalDB")  # Default to local DB
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://dkavita2121:Slappy132@cluster0.dwumgci.mongodb.net/journalDB?retryWrites=true&w=majority&appName=Cluster0")  # Default to local DB
